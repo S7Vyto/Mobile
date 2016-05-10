08b292e6de2a131b80a3bb0847cf0b7db1509fc3
Semyon Vyatkin <samvyto@gmail.com>
2016-05-10 21:30:58 +0500

    Initialization project - VK

---
+++ iOS/VK/VK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VK
+//
+//  Created by Semyon Vyatkin on 10/05/16.
+//  Copyright © 2016 Semyon-Vyatkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/VK/VK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VK
+//
+//  Created by Semyon Vyatkin on 10/05/16.
+//  Copyright © 2016 Semyon-Vyatkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
